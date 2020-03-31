0723db99fe93318076248fae7f3ffa3d42fca296
Dalton DelPiano <dalton.delpiano@ibm.com>
2020-03-31 12:57:29 -0400

    fix: update script to check for necessary variables

--- bin/vault_load.sh
+++ bin/vault_load.sh
@@ -18,5 +18,39 @@ while getopts ":e:s:a:" opt; do
   esac
 done
 
+if [[ -z "$VAULT_PROJECT" ]]; then
+    echo "Must provide VAULT_PROJECT in environment" 1>&2
+    ERROR=1
+fi
+
+if [[ -z "$VAULT_TOKEN" ]]; then
+    echo "Must provide VAULT_TOKEN in environment" 1>&2
+    ERROR=1
+fi
+
+if [[ -z "$VAULT_ADDR" ]]; then
+    echo "Must provide VAULT_ADDR in environment" 1>&2
+    ERROR=1
+fi
+
+if [[ -z "$VAULT_ENV" ]]; then
+    echo "Must provide -e (environment) option" 1>&2
+    ERROR=1
+fi
+
+if [[ -z "$VAULT_APPLICATION" ]]; then
+    echo "Must provide -a (application name) option" 1>&2
+    ERROR=1
+fi
+
+if [[ -z "$SECRET_NAME" ]]; then
+    echo "Must provide -s (secret name) option" 1>&2
+    ERROR=1
+fi
+
+if [[ "$ERROR" == 1 ]]; then
+    exit 1
+fi
+
 OUTPUT=$(curl --fail -X GET "${VAULT_ADDR}/v1/generic/project/${VAULT_PROJECT}/${VAULT_ENV}/${VAULT_APPLICATION}/${SECRET_NAME}" -H "X-Vault-Token: ${VAULT_TOKEN}")
 echo "$OUTPUT" | jq -r '.data.value'
